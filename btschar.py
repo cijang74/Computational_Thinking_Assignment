88d0cc3b70ce20e8c0d23a7b307b6a1cee0488d1
cijang74 <82044367+cijang74@users.noreply.github.com>
2021-12-02 07:44:22 +0900

    asf

--- btschar.py
+++ btschar.py
@@ -1,9 +1,9 @@
 btschars = {"진" : "RJ", "슈가" : "Shooky", "제이홉" : "Mang", "RM" : "Koya", "지민" : "Chimmy", "뷔" : "Tata", "정국": "Cooky"}
 
 while True:
-    myBTS = input(str(list(btschars.keys())) + "중 좋아하는 멤버는?")
+    myBTS = input(str(list(btschars.keys())) + "중 좋아하는 멤버는?: ")
     if myBTS in btschars:
-        print("<%s>의 캐릭터 인형은 <%s>입니다." %(myBTS, btschars[myBTS]))
+        print("<%s>의 캐릭터 인형은 <%s>입니다.\n" %(myBTS, btschars[myBTS]))
     elif myBTS == "끝":
         break
     else:
